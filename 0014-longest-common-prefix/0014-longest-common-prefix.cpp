4c33715124e11d583a34b530c4fb782afdcabea3
airimatt <116922204+airimatt@users.noreply.github.com>
2022-11-29 19:30:18 -0500

    Time: 6 ms (76.81%), Space: 9.3 MB (53.35%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        
+        string lcp = strs[0];
+        
+        for (int i = 1; i < strs.size(); i++) {
+            if (lcp.length() == 0) {
+                return "";
+            }
+            if (lcp.length() > strs[i].length()) {
+                // lcp.erase(strs[i].length(), lcp.length() - strs[i].length());
+                lcp = lcp.substr(0, strs[i].length());
+            }
+            for (int j = 0; j < strs[i].length(); j++) {
+                if (strs[i][j] != lcp[j]) {
+                    // lcp.erase(j, strs[i].length() - j);
+                    lcp = lcp.substr(0, j);
+                }
+            }
+        }
+        
+        return lcp;
+    }
+};

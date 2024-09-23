db7f1a3255733700dccdc02e02656bab7bd4bf96
Airi Matsushita <116922204+airimatt@users.noreply.github.com>
2024-09-23 12:30:42 -0400

    Time: 323 ms (13.11%) | Memory: 18.2 MB (14.26%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.py
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -15,18 +15,3 @@ class Solution:
                 l = mid + 1
         
         return k
-
-
-
-        # while l <= r:
-        #     k = (l + r) // 2
-
-        #     totalTime = 0
-        #     for p in piles:
-        #         totalTime += math.ceil(float(p) / k)
-        #     if totalTime <= h:
-        #         res = k
-        #         r = k - 1
-        #     else:
-        #         l = k + 1
-        # return res
